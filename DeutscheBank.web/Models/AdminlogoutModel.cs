be71759bed845ba9304b79c2c95dd44a965b242d
brudlazl <brudlazl@PC001999FD02BD>
2016-12-12 17:05:36 +0100

    LoginLogout fehlerhaft

---
+++ DeutscheBank.web/Models/AdminlogoutModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DeutscheBank.web.Models
+{
+    public class AdminLogoutModel
+    {
+
+        [Required]
+        [StringLength(3, ErrorMessage = "yes or no")]
+        public string LogoutAdmin { get; set; }
+
+    }
+}
+
+
+
+
